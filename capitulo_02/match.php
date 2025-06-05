413cf1879116395d452ffa1e806e32c743d61662
Rosa <costurezati@gmail.com>
2025-06-05 06:51:30 -0300

    .

---
+++ capitulo_02/match.php
@@ -0,0 +1,33 @@
+<?php
+$day = 'Quarta';
+
+switch ($day){
+    case 'Segunda':
+        $offer = '20% de desconto nos chocolates';
+        break;
+    case 'Terça':
+        $offer = '20% de desconto no caramelo';
+        break;
+    default:
+        $offer = 'Compre 3, leve um grátis';
+}
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Candy Store</title>
+    <link rel="stylesheet" href="./ressources/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <section class="hero" id="hero">
+        <h1>The Candy Store</h1>
+        <h2>Ofertas da <?=$day?></h2>
+        <h2><?=$offer?></h2>        
+    </section>
+    
+</body>
+</html>
---
+++ capitulo_02/usandoifelsif.php
@@ -0,0 +1,30 @@
+<?php
+$stock = 5;
+$ordered = 3; 
+
+if ($stock > 0){
+    $message = 'Tem em estoque.';
+}elseif($ordered > 0){
+    $message = 'Chega no estoque em breve';
+}else{
+    $message = 'Acabou';
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Candy Store</title>
+    <link rel="stylesheet" href="./ressources/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <section class="hero" id="hero">
+        <h1>The Candy Store</h1>
+        <h2>Chocolate</h2>
+        <h2><?=$message?></h2>        
+    </section>
+    
+</body>
+</html>
