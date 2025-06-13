26783bef4ed9b3fe657185c93061b5fcc455dd17
Rosa <costurezati@gmail.com>
2025-06-13 06:11:26 -0300

    .

---
+++ capitulo_03/includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Candy Store</title>
+</head>
+<body>
+    <header>
+        <h1>The Candy Store</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="candy.php">Candy</a>
+            <a href="about.php">Sobre</a>
+            <a href="contact.php">Contato</a>
+        </nav>
+    </header>
+</body>
+</html>
