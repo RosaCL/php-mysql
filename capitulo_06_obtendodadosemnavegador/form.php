1e00ee0f5d7003e51e143ac8b072b72f2b0c3b7e
Rosa <costurezati@gmail.com>
2025-07-01 06:03:52 -0300

    .

--- capitulo_06_obtendodadosemnavegador/form.php
+++ capitulo_06_obtendodadosemnavegador/form.php
@@ -1,21 +1,17 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Art Store</title>
-    <link rel="stylesheet" href="ressources/css/style.css">
+    <link rel="stylesheet" href="./ressources/css/style.css">
 </head>
 <body>
-    <header>
-        <img src="./ressources/img/logo-sembg.png" alt="">
-        <nav>
-            <a href="#home">Home</a>
-            <a href="#perfil">Perfil</a>
-            <a href="#contato">Contato</a>
-        </nav>
-    </header>
-    <section>
+    <?php include './includes/header.php' ?>
+        <section>
         <div class="box">
             <div class="formcontainer">
                 <form action="">
@@ -32,11 +28,11 @@
                     </p>
                     <p>Avaliação:</p>
                     <div class="avalia">
-                            <p>1<input type="radio" name="av" value="1">&nbsp;</p>
-                            <p>2<input type="radio" name="av" value="2">&nbsp;</p> 
-                            <p>3<input type="radio" name="av" value="3"> &nbsp;</p>                   
+                            <p>1 <input type="radio" name="av" value="1">&nbsp;</p>
+                            <p>2 <input type="radio" name="av" value="2">&nbsp;</p> 
+                            <p>3 <input type="radio" name="av" value="3"> &nbsp;</p>                   
                     </div>
-                    <p><input type="checkbox" name="terms" value="true">Eu concordo com os termos e condições</p>
+                    <p><input type="checkbox" name="terms" value="true"> Eu concordo com os termos e condições</p>
                     <input class="btn" type="submit" value="Enviar">
                 </form>
             </div>
@@ -44,9 +40,6 @@
 
     </section>
 
-    <footer>
-        <a href="https://github.com/RosaCL"><img src="ressources/img/costureza.png" alt=""></a>
-    </footer>
-    
+    <?php include './includes/footer.php'?>
 </body>
 </html>
