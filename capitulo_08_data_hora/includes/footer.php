342c9de30bfcb7f3a3947b1b7073718065d066bc
Rosa <costurezati@gmail.com>
2025-07-11 07:00:17 -0300

    .

---
+++ capitulo_08_data_hora/includes/footer.php
@@ -0,0 +1,5 @@
+
+    <footer>
+        <a href="https://github.com/RosaCL"><img src="ressources/img/costureza.png" alt=""></a>
+    </footer>
+    
---
+++ capitulo_08_data_hora/includes/header.php
@@ -0,0 +1,10 @@
+
+    <header>
+        <img src="./ressources/img/logo-sembg.png" alt="">
+        <nav>
+            <a href="#home">Home</a>
+            <a href="#perfil">Perfil</a>
+            <a href="#contato">Contato</a>
+        </nav>
+    </header>
+
